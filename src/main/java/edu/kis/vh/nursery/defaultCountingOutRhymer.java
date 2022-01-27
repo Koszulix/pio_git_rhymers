1be5acaef48855cb5cbed47896ea18fe916d6284
Koszulix <legon11@wp.pl>
2022-01-27 18:45:01 +0100

    1.4.1 accessories and hermetization - Changed public status

--- src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
@@ -2,14 +2,15 @@ package edu.kis.vh.nursery;
 
 public class defaultCountingOutRhymer {
 
-    public static final int MAX_SIZE = 12;
-    public static final int MAX_SIZE2 = MAX_SIZE - 1;
+    private static final int MAX_SIZE = 12;
+    private static final int MAX_SIZE2 = MAX_SIZE - 1;
     private final int[] numbers = new int[MAX_SIZE];
 
-    public static final int EMPTY_TOTAL_RHYMER = -1;
-    public int total = EMPTY_TOTAL_RHYMER;
+    private static final int EMPTY_TOTAL_RHYMER = -1;
+    private int total = EMPTY_TOTAL_RHYMER;
 
-    public static final int DEFAULT_RETURN = -1;
+    private static final int DEFAULT_RETURN = -1;
+
     public void countIn(int in) {
         if (!isFull())
             numbers[++total] = in;
